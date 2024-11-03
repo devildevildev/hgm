74433bb98a4973ea32d8a7a27ddd19b669095cff
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-11-03 11:28:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,7 +83,9 @@ async def Lazy_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    # await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+    chat = await LazyPrincessBot.get_chat(LOG_CHANNEL)
+    await LazyPrincessBot.send_message(chat_id=chat.id, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
